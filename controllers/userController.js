5e5d75589f57d6e0c4739c604179dd4d74648d34
Joel Wasserman <joel.wasserman8@gmail.com>
2018-02-15 13:48:51 -0500

    working on update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,13 @@ module.exports = {
   createUser: (req, res) => {
     // validate params
     const params = pluck(['email', 'password', 'firstName', 'lastName', 'birthday'], req.body).end()
-    if (params.length != 5) return res.status(500).json({ success: false, message: helper.strings.invalidParameters })
+    if (params.length != 5) return res.status(200).json({ success: false, message: helper.strings.invalidParameters })
+
+    // convert time stamp
+    params.birthday = new Date(params.birthday)
 
     // validate password length
-    if (params.password.length < 5) return res.status(500).json({ success: false, message: helper.strings.invalidPasswordParameter })
+    if (params.password.length < 5) return res.status(200).json({ success: false, message: helper.strings.invalidPasswordParameter })
 
     return sqlModels.User.findOrCreate({ where: { email: params.email }, defaults: params })
       .then(result => {
@@ -24,7 +27,30 @@ module.exports = {
       })
   },
   updateUser: (req, res) => {
+    // validate params
+    const params = pluck(['email', 'password', 'firstName', 'lastName', 'birthday', 'recommendationsGiven', 'recommendationsReceived', 'recommendationsGivenCorrect', 'recommendationsReceivedCorrect'])
+    if (!params.email) return res.status(500).json({ success: false, message: helper.strings.invalidParameters })
 
+    // convert birthday
+    if (params.birthday) params.birthday = new Date(params.birthday)
+
+    // find the user by the email
+    sqlModels.User.findOne({ where: { email: params.email }})
+      .then(user => {
+        // check if a user was returned
+        if (!user) return res.status(200).json({ success: false, message: helper.strings.noUserExistingByThatEmail })
+        // update the user properties
+        Object.keys(params).forEach(updatedProperty => {
+          user[updatedProperty] = params[updatedProperty]
+        })
+        return user.save()
+      })
+      .then(updatedUser => {
+        return res.status(200).json({ success: true, message: helper.strings.userUpdatedSuccesfully, user: updatedUser })
+      })
+      .catch(e => {
+        return res.status(500).json({ success: false, message: helper.strings.anErrorHappened })
+      })
   },
   getUsers: (req, res) => {
 
--- helper/strings.js
+++ helper/strings.js
@@ -6,5 +6,7 @@ module.exports = {
   // user
   invalidPasswordParameter: 'Password must be greater than 5 characters long',
   userAlreadyExists: 'A user already exists with this email',
-  userCreatedSuccesfully: 'New user created succesfully'
+  userCreatedSuccesfully: 'New user created succesfully',
+  noUserExistingByThatEmail: 'No user exists by that email',
+  userUpdatedSuccesfully: 'The user was updated succesfully'
 }
