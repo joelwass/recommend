9918c05103e9f1fb639ae6148e1039917687b443
Joel Wasserman <joel.wasserman8@gmail.com>
2018-03-21 09:46:22 -0400

    added splash page stuff

--- client/pages/index.js
+++ client/pages/index.js
@@ -64,17 +64,28 @@ class Index extends React.Component {
 
   splashPage () {
     return (
-      <div className='center header__main'>
-        <h1>Welcome To Recommend!</h1>
+      <div>
+        <div className='center header__main'>
+          <h1>Welcome To Recommend!</h1>
+        </div>
+        <div className='center'>
+          <p className='splash'>Recommend is an application built with the desire to remove user bias when receiving recommendations.</p>
+          <p className='splash'>We live in a world built by <a href='https://tinyurl.com/y82zs8k3'>echo chambers</a>. Through simple human nature, we tend to surround ourselves with like minded people. You get your recommended food from chefs you've historically enjoyed. You get your book recommendations from friends who like similar books. That all makes sense, but does it increase the breadth of your experiences? Do you gain insight into the <a href='https://tinyurl.com/ybnzwobw'>other side</a>? Or are you reinforcing your prior conceived opinions? Are you pigeon-holing yourself?</p>
+          <p className='splash'>As <a href='https://tinyurl.com/387qha'>Jonathan Gardner</a> wrote, "We can't write off the danger of complacency, growing rigidity, imprisonment by our own comfortable habits and opinions".</p>
+          <p className='splash'>And he's right. So we built Recommend. You now can get (and give) anonymized recommendations meant to expand your mind without introducing prior recommender bias. If a Trump supporter loves a book, but you know they're a Trump supporter and you're a diehard democrat, will you read the book? Probably not, regardless if you'd like the book in the end.</p>
+          <p className='splash'>Complete your recommendations and gain points, give well received recommendations and gain badges. Maybe one day you'll be a certified Recommender. Follow users you think will give good recommendations, or just test your friendships by recommending what you think they'd like and seeing how they do.</p>
+        </div>
       </div>
     )
   }
 
   componentDidMount () {
     // animate header in 2 seconds, animation should last half a second
-    setTimeout(() => {
-      this.props.setShowedAnimation()
-    }, 1500)
+    if (this.props.authenticated) {
+      setTimeout(() => {
+        this.props.setShowedAnimation()
+      }, 1500)
+    }
   }
 
   componentWillReceiveProps (props) {
